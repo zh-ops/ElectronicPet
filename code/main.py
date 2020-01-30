643984cefb9695a1ba444e1b4ebe1e4e1456d0ce
ttzc <2633941738@qq.com>
2020-01-30 14:22:44 +0800

    显示宠物，顺便修改图像渲染模式

--- code/main.py
+++ code/main.py
@@ -4,13 +4,17 @@ def main():
     root.title("电子宠物")
     root.geometry('960x684')
 
-    bg_path = '../picture/bg.gif'
-    bg_open = tk.PhotoImage(file=bg_path)
-    bg = tk.Label(root, image=bg_open, compound=tk.CENTER)
-    bg.pack()
+    #bg_path = '../picture/bg.gif'
+    #bg_open = tk.PhotoImage(file=bg_path)
+    #bg = tk.Label(root, image=bg_open, )
+    #bg.pack()
+    #背景会遮住元素，去掉。
 
     dog_path = '../picture/normal_dog.gif'
-
+    dog_photo = tk.PhotoImage(file=dog_path)
+    dog = tk.Label(root, image=dog_photo)
+    dog.pack(side=tk.LEFT)
+    
     root.mainloop()
 if __name__ == "__main__":
     main()
