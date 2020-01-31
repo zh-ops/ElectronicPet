1de0749b04390e7ca4f16a231dc5fb242a404acc
ttzc <2633941738@qq.com>
2020-01-31 14:47:52 +0800

    完善vox文件夹的自述文件

--- code/main.py
+++ code/main.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+
+
 def main():
     root = tk.Tk()
     root.title("电子宠物")
@@ -7,14 +9,16 @@ def main():
     #bg_path = '../picture/bg.gif'
     #bg_open = tk.PhotoImage(file=bg_path)
     #bg = tk.Label(root, image=bg_open, )
-    #bg.pack()
-    #背景会遮住元素，去掉。
+    # bg.pack()
+    # 背景会遮住元素，去掉。
 
     dog_path = '../picture/normal_dog.gif'
     dog_photo = tk.PhotoImage(file=dog_path)
     dog = tk.Label(root, image=dog_photo)
     dog.pack(side=tk.LEFT)
-    
+
     root.mainloop()
+
+
 if __name__ == "__main__":
     main()
--- vox/README.md
+++ vox/README.md
@@ -1,1 +1,4 @@
 # vox文件夹
+
+## 用处
+这个项目所有素材均使用MagicaVoxel绘制,这是MagicaVoxel的项目文件，方便修改.
